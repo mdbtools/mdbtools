00adede0d06f2f641e31302b0153575d1ec21a09
brianb <brianb>
2004-01-10 21:54:59 +0000

    Fix bug 659762, typo.

--- autogen.sh
+++ autogen.sh
@@ -12,7 +12,7 @@ DIE=0
 
 (autoconf --version) < /dev/null > /dev/null 2>&1 || {
   echo
-  echo "**Error**: You must have \`autoconf' installed to."
+  echo "**Error**: You must have \`autoconf' installed."
   echo "Download the appropriate package for your distribution,"
   echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
   DIE=1
