2606b8eb51b3cfa8c4b6b003e70c8bdd57f9b4b8
whydoubt <whydoubt>
2005-03-16 12:50:21 +0000

    Uncomment use of OLE functions

--- src/util/mdb-prop.c
+++ src/util/mdb-prop.c
@@ -20,27 +20,20 @@
 
 #include "mdbtools.h"
 
-#define MSYSOBJECTS "MSysObjects"
-#define LVPROP "Lv"
-
 #undef MDB_DEBUG
 //#define MDB_DEBUG 1
 
-void dump_kkd(gchar *kkd, int len);
-
-MdbHandle *mdb;
+void dump_kkd(MdbHandle *mdb, gchar *kkd, int len);
 
 int
 main(int argc, char **argv)
 {
-	int len, pos, col_num;
-	MdbColumn *col;
+	MdbHandle *mdb;
 	MdbTableDef *table;
 	gchar name[256];
 	gchar buf[MDB_BIND_SIZE];
-	gchar kkd_pg[200000];
-	gchar kkd_ptr[MDB_MEMO_OVERHEAD+1];
-
+	int col_num;
+	int found = 0;
 
 	/* 
 	** optind is now the position of the first non-option arg, 
@@ -48,15 +41,16 @@ main(int argc, char **argv)
 	*/
 	if (argc < 2) {
 		fprintf(stderr,"Usage: %s <file> <name> [<prop col>]\n",argv[0]);
-		exit(1);
+		return 1;
 	}
 
 	mdb_init();
 
 	if (!(mdb = mdb_open(argv[optind], MDB_NOFLAGS))) {
-		exit(1);
+		mdb_exit();
+		return 1;
 	}
-	table = mdb_read_table_by_name(mdb, MSYSOBJECTS, MDB_ANY);
+	table = mdb_read_table_by_name(mdb, "MSysObjects", MDB_ANY);
 	mdb_read_columns(table);
 	mdb_rewind_table(table);
 
@@ -65,34 +59,42 @@ main(int argc, char **argv)
 
 	while(mdb_fetch_row(table)) {
 		if (!strcmp(name, argv[optind+1])) {
-			memcpy(kkd_ptr, buf, MDB_MEMO_OVERHEAD);
-			col=g_ptr_array_index(table->columns,col_num - 1);
-/*
-			len = mdb_ole_read(mdb, col, kkd_ptr, MDB_BIND_SIZE);
-			memcpy(kkd_pg, buf, len);
-			pos = len;
-			while ((len = mdb_ole_read_next(mdb, col, kkd_ptr))) {
-				memcpy(&kkd_pg[pos], buf, len);
-				pos += len;
-			}
-*/
-			len = pos;
-			buffer_dump(buf, 0, MDB_MEMO_OVERHEAD);
-			//dump_kkd(kkd_pg, len);
+			found = 1;
+			break;
 		}
+	}
+
+	if (found) {
+		MdbColumn *col;
+		gchar kkd_ptr[MDB_MEMO_OVERHEAD];
+		gchar *kkd_pg = g_malloc(200000);
+		int len, pos;
+
+		memcpy(kkd_ptr, buf, MDB_MEMO_OVERHEAD);
+		col = g_ptr_array_index(table->columns, col_num - 1);
+		len = mdb_ole_read(mdb, col, kkd_ptr, MDB_BIND_SIZE);
+		memcpy(kkd_pg, buf, len);
+		pos = len;
+		while ((len = mdb_ole_read_next(mdb, col, kkd_ptr))) {
+			memcpy(kkd_pg + pos, buf, len);
+			pos += len;
+		}
+		len = pos;
+		dump_kkd(mdb, kkd_pg, len);
+		g_free(kkd_pg);
 	}
 
 	mdb_free_tabledef(table);
 	mdb_close(mdb);
 	mdb_exit();
 
-	exit(0);
+	return 0;
 }
 void print_keyvalue(gpointer key, gpointer value, gpointer user_data)
 {
 		printf("%s = %s\n", (gchar *)key, (gchar *)value);
 }
-void dump_kkd(gchar *kkd, int len)
+void dump_kkd(MdbHandle *mdb, gchar *kkd, int len)
 {
 	guint32 record_len, record_type;
 	int pos = 0;
