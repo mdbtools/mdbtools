cb0ee059b29519a982fe298e58c7e5d7e88d1cca
Evan Miller <emmiller@gmail.com>
2020-12-21 15:42:38 -0500

    Migrate from Travis CI to GitHub Actions (#229)
    * Add GitHub Actions workflow
    
    * Consolidate CI test commands into test_script.sh
    
    * Remove Travis build

---
+++ test_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+# Simple test script; run after performing
+# git clone https://github.com/mdbtools/mdbtestdata.git test
+./src/util/mdb-json test/data/ASampleDatabase.accdb "Asset Items"
+./src/util/mdb-json test/data/nwind.mdb "Customers"
+./src/util/mdb-count test/data/ASampleDatabase.accdb "Asset Items"
+./src/util/mdb-count test/data/nwind.mdb "Customers"
+./src/util/mdb-prop test/data/ASampleDatabase.accdb "Asset Items"
+./src/util/mdb-prop test/data/nwind.mdb "Customers"
+./src/util/mdb-schema test/data/ASampleDatabase.accdb
+./src/util/mdb-schema test/data/nwind.mdb
+./src/util/mdb-tables test/data/ASampleDatabase.accdb
+./src/util/mdb-tables test/data/nwind.mdb
+./src/util/mdb-ver test/data/ASampleDatabase.accdb
+./src/util/mdb-ver test/data/nwind.mdb
+./src/util/mdb-sql -i test/sql/nwind.sql test/data/nwind.mdb
+./src/util/mdb-queries test/data/ASampleDatabase.accdb qryCostsSummedByOwner
