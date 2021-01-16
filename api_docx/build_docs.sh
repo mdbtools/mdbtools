87526e08073200fc8d6a1382a1b6244e7324324a
pedromorgan <pedromorgan@gmail.com>
2021-01-16 04:51:40 +0000

    make build cleaner

--- api_docx/build_docs.sh
+++ api_docx/build_docs.sh
@@ -4,10 +4,12 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT="$(dirname "$DIR")"
 
+echo "cleanup"
 rm -f -r $ROOT/temp-man-pages
 
 mkdir $ROOT/temp-man-pages
 
+echo "create man pages"
 $ROOT/api_docx/pre_build.py
 
 doxygen $ROOT/api_docx/doxygen.conf
