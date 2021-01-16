00332fe22e464ee6bcc6edc084ba397e9439ff0f
pedromorgan <pedromorgan@gmail.com>
2021-01-16 05:31:23 +0000

    pre build python

--- api_docx/build_docs.sh
+++ api_docx/build_docs.sh
@@ -8,13 +8,16 @@ echo "DIR = $DIR"
 ROOT="$(dirname "$DIR")"
 echo "ROOT = $ROOT"
 
-echo "cleanup $ROOT/temp-man-pages"
+echo "### cleanup $ROOT/temp-man-pages"
 rm -f -r $ROOT/temp-man-pages
 
 mkdir $ROOT/temp-man-pages
 
-echo "create man pages"
+echo "### create man pages"
 which python3
 python3 $ROOT/api_docx/pre_build.py
+
+echo "### doxygen version"
+doxygen -v
 
 doxygen $ROOT/api_docx/doxygen.conf
